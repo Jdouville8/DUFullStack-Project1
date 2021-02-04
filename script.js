578a56afb386960a4a7ce205de8810094484499e
Justin Douville <jdouville8@gmail.com>
2021-02-04 13:43:19 -0500

    Got floating action button to dsiplay on page, no functionality yet

--- script.js
+++ script.js
@@ -55,9 +55,6 @@ $(document).ready(function () {
   });
 
   // Floating Action Button
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, options);
-  });
+  $('.fixed-action-btn').floatingActionButton();
 
 });
